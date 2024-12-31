8d841b4fd915154d084c2e9893710cf39d897a8d
Youssef Alboghdady <youssefalboghdady399@gmail.com>
2024-12-31 13:17:53 +0200

    drop-downlist_ radioButtons

---
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
@@ -0,0 +1,58 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+
+import com.luv2code.springboot.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class StudentController {
+
+    // add a new field for countries saved in application.properties
+    @Value("${countries}")
+    private List<String> countries;
+
+    // add a new field for programming languages saved in application.properties
+    @Value("${languages}")
+    private List<String> languages;
+
+    // define a method to show the initial HTML form
+    @GetMapping("/showStudentForm")
+    public String showForm(Model model) {
+        // create a student object
+        Student theStudent = new Student();
+
+        // add theStudent to the model
+        model.addAttribute("student", theStudent);
+
+        // add the list of countries to the model
+        model.addAttribute("countries", countries);
+
+        // add the list of programming languages to the model
+        model.addAttribute("languages", languages);
+
+        return "student-form";
+    }
+
+
+    // define a method to process the HTML form
+    @PostMapping("/processStudentForm")
+    public String processForm(@ModelAttribute("student") Student theStudent) {
+        // log the input data
+        System.out.println("The Student: " +
+                theStudent.getFirstName() + " " +
+                theStudent.getLastName()) ;
+
+        return "student-confirmation";
+    }
+
+
+
+
+}
---
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/model/Student.java
@@ -0,0 +1,38 @@
+package com.luv2code.springboot.thymeleafdemo.model;
+
+public class Student {
+
+    private String firstName;
+    private String lastName;
+    private String country;
+    private String programmingLanguage;
+
+    public Student() {}
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getCountry() {
+        return country;
+    }
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    public String getProgrammingLanguage() {
+        return programmingLanguage;
+    }
+    public void setProgrammingLanguage(String programmingLanguage) {
+        this.programmingLanguage = programmingLanguage;
+    }
+
+
+}
